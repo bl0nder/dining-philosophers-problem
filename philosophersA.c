a046f96b273d9a88cd63b6ff063a89c27be46560
bl0nder <goelpalaash@gmail.com>
2022-12-18 17:15:14 +0530

    .

--- philosophersA.c
+++ philosophersA.c
@@ -22,9 +22,9 @@ int getRightFork(int philNum) {
 }
 
 void eat(int philNum) {
-    //Atomic operation of eating - 2 seconds (say)
+    //Atomic operation of eating - 1 seconds (say)
     printf("Philosopher %d is eating\n", philNum);
-    sleep(0);
+    sleep(1);
 }
 
 void getForks(int philNum) {
@@ -50,7 +50,7 @@ void putForks(int philNum) {
 void think(int philNum) {
     //Think for 1 second 
     printf("Philosopher %d is thinking\n", philNum);
-    sleep(0);
+    sleep(1);
 }
 
 void *doSomething(void* args) {
